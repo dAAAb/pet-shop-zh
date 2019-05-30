f93b19642f4f84de8efea8a86d775841475c2abf
daaab <dablog@gmail.com>
2019-05-31 02:38:35 +0800

    fix atvImg

--- src/js/app.js
+++ src/js/app.js
@@ -10,15 +10,16 @@ App = {
 
       for (i = 0; i < data.length; i ++) {
         petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.atvImg-layer').attr('data-img', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
         petTemplate.find('.pet-age').text(data[i].age);
         petTemplate.find('.pet-location').text(data[i].location);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-        petTemplate.find('.atvImg-rendered-layer').attr('style', style='background-image: url("'+data[i].picture+'");');
+        //petTemplate.find('.atvImg-rendered-layer').attr('style', style='background-image: url("'+data[i].picture+'");');
 
         petsRow.append(petTemplate.html());
       }
+      atvImg();
     });
 
     return await App.initWeb3();
